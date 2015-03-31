70c26f613ceb4ebc8748c54ee9b9d68b0cefc224
mhedbom <mathias.hedbom@ha.ax>
2015-03-31 09:40:51 +0300

    Removed unused include

--- testCourseCalculation.cpp
+++ testCourseCalculation.cpp
@@ -2,8 +2,6 @@
 #include "catch.hpp"
 #include "CourseCalculation.h"
 
-#include <iostream>
-
 TEST_CASE("CourseCalculationTest")
 {
 	SECTION("Test CTS is BTW unless course is within TACK_ANGLE degrees from TWD, if not tack")
@@ -60,7 +58,7 @@ TEST_CASE("CourseCalculationTest")
 
 	SECTION("Test calculateDTW with 3 different poisitions for waypoint and boat")
 	{
-   		const int noOfPositions = 3;
+   		const int noOfDTW = 3;
 		double boatLats [3] 		= {60.836881, 60.073063, 59.539888 };
 		double boatLongs [3] 		= {19.143219, 22.185974, 19.894409 };
 		double waypointLats [3] 	= {60.103333, 60.103172, 60.103362 };
@@ -69,12 +67,11 @@ TEST_CASE("CourseCalculationTest")
 		/* Resulting distance between boat and waypont in meters.
 		 * Taken from http://www.movable-type.co.uk/scripts/latlong.html 
 		 */
-		const double result_distance[3] = { 92218.1699654549,
-										    125222.8858237675,
-										    62684.5898278684 };
+		const double result_distance[3] = { 92218.1699654549, 125222.8858237675, 62684.5898278684 };
+
 		CourseCalculation cc;
 
-		for(int i = 0; i < noOfPositions; i++) {
+		for(int i = 0; i < noOfDTW; i++) {
 			cc.calculateDTW(boatLats[i], boatLongs[i], waypointLats[i], waypointLongs[i]);
 			REQUIRE(std::fabs(result_distance[i] - cc.getDTW()) < 0.1);
 		}
